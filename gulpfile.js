fdd78b3205024958cb30470bda275f631d0cbf98
MihailSpiridonov <84262885+MihailSpiridonov@users.noreply.github.com>
2021-09-08 08:57:43 +0300

    Выполнил автоматизацию html и css

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,14 @@ const sourcemap = require("gulp-sourcemaps");
 const less = require("gulp-less");
 const postcss = require("gulp-postcss");
 const autoprefixer = require("autoprefixer");
+const htmlmin = require('gulp-htmlmin');
+const csso = require('postcss-csso');
+const rename = require('gulp-rename');
+const del = require('del');
+const squoosh = require('gulp-libsquoosh');
+const svgstore = require('gulp-svgstore');
+const terser = require('gulp-terser');
+const webp = require('gulp-webp');
 const sync = require("browser-sync").create();
 
 // Styles
@@ -14,21 +22,57 @@ const styles = () => {
     .pipe(sourcemap.init())
     .pipe(less())
     .pipe(postcss([
-      autoprefixer()
+      autoprefixer(),
+      csso()
     ]))
+    .pipe(rename("style.min.css"))
     .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("source/css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(sync.stream());
 }
 
 exports.styles = styles;
+
+// html
+const html = () => {
+  return gulp.src("source/*.html")
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest("build"));
+}
+
+// Scripts
+const scripts = () => {
+  return gulp.src ("source/js/*.js")
+    .pipe(terser())
+    .pipe(rename(".min.js"))
+    .pipe(gulp.dest("build/js"))
+    .pipe(sync.stream());
+}
+
+exports.scripts = scripts;
+/* const scripts = () => {
+  return gulp.src("source/js/script.js")
+    .pipe(terser())
+    .pipe(rename("script.min.js"))
+    .pipe(gulp.dest("build/js"))
+    .pipe(sync.stream());
+}
+
+exports.scripts = scripts; */
+
+// Images
+//const images = () => {
+//  return gulp.src("sourse/img/**/*.img")
+//  .pipe()
+//  .pipe();
+//}
 
 // Server
 
 const server = (done) => {
   sync.init({
     server: {
-      baseDir: 'source'
+      baseDir: 'build'
     },
     cors: true,
     notify: false,
@@ -47,5 +91,5 @@ const watcher = () => {
 }
 
 exports.default = gulp.series(
-  styles, server, watcher
+  styles, html, scripts, server, watcher
 );
