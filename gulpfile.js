0f3b3bb15cf6e8646642ddd76c30171eff8a7667
MihailSpiridonov <84262885+MihailSpiridonov@users.noreply.github.com>
2021-09-11 15:57:50 +0300

    Закончил сборку проекта

--- gulpfile.js
+++ gulpfile.js
@@ -51,16 +51,65 @@ const scripts = () => {
 exports.scripts = scripts;
 
 // Images
-const images = () => {
+const optimizeImages = () => {
   return gulp.src("source/img/**/*.{jpg,png,svg}")
-  .pipe(squoosh())
-  .pipe(gulp.dest("build/img"));
+    .pipe(squoosh())
+    .pipe(gulp.dest("build/img"));
 }
 
-exports.images = images;
+exports.optimizeImages = optimizeImages;
+
+const copyImages = () => {
+  return gulp.src("source/img/**/*.{jpg,png,svg}")
+    .pipe(gulp.dest("build/img"));
+}
+
+exports.copyImages = copyImages;
+
+// Webp
+const createWebp = () => {
+  return gulp.src("source/img/**/*.{jpg,png}")
+    .pipe(webp({quality: 90}))
+    .pipe(gulp.dest("build/img"));
+}
+
+exports.createWebp = createWebp;
+
+// Sprite
+const sprite = () => {
+  return gulp.src("source/img/sprite/*.svg")
+    .pipe(svgstore({
+      inlineSvg: true
+    }))
+    .pipe(gulp.dest("build/img/sprite"))
+}
+
+exports.sprite = sprite;
+
+// Copy
+
+const copy = (done) => {
+  gulp.src([
+    "source/fonts/**/*.{woff2,woff}",
+    "*.ico",
+    "source/img/**/*.svg",
+    "!source/img/sprite/*.svg",
+    "manifest.webmanifest"
+  ], {
+    base: "source"
+  })
+    .pipe(gulp.dest("build"))
+  done();
+}
+
+exports.copy = copy;
+
+// Clean
+const clean = () => {
+  return del("build");
+}
 
 // Server
-
 const server = (done) => {
   sync.init({
     server: {
@@ -75,13 +124,50 @@ const server = (done) => {
 
 exports.server = server;
 
-// Watcher
-
-const watcher = () => {
-  gulp.watch("source/less/**/*.less", gulp.series("styles"));
-  gulp.watch("source/*.html").on("change", sync.reload);
+// Reload
+const reload = (done) => {
+  sync.reload();
+  done();
 }
 
-exports.default = gulp.series(
-  styles, html, scripts, server, watcher
+// Watcher
+const watcher = () => {
+  gulp.watch("source/less/**/*.less", gulp.series(styles));
+  gulp.watch("source/js/script.js", gulp.series(scripts));
+  gulp.watch("source/*.html", gulp.series(html, reload));
+}
+
+// Build
+const build = gulp.series(
+  clean,
+  copy,
+  optimizeImages,
+  gulp.parallel(
+    styles,
+    html,
+    scripts,
+    sprite,
+    createWebp
+  ),
 );
+
+exports.build = build;
+
+// Default
+
+
+exports.default = gulp.series (
+  clean,
+  copy,
+  copyImages,
+  gulp.parallel(
+    styles,
+    html,
+    scripts,
+    sprite,
+    createWebp
+  ),
+  gulp.series(
+    server,
+    watcher
+));
